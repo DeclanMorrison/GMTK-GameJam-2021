cc8a0b0aac58bc558ff3ac4e8d558e634cf530e6
George Philbrick <gphilbri@gmail.com>
2021-06-12 00:17:49 -0400

    Added glitch effect from quantum entanglement

--- GMTK GameJam 2021/Assets/Scripts/GameManager.cs
+++ GMTK GameJam 2021/Assets/Scripts/GameManager.cs
@@ -9,12 +9,14 @@ public class GameManager : MonoBehaviour
     public Camera overgrowthCamera;
 
     public SuperPositionState superPositionState = SuperPositionState.TOGETHER;
-
     public static GameManager instance;
+    GlitchEffects[] glitches;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        glitches = GetComponentsInChildren<GlitchEffects>();
     }
 
     // Update is called once per frame
@@ -24,28 +26,76 @@ public class GameManager : MonoBehaviour
         {
             if (superPositionState == SuperPositionState.TOGETHER) 
             {
-                SplitDimensions();
+                StartCoroutine("SplitDimensions");
             }
             else
             {
-                JoinDimensions();
+                StartCoroutine("JoinDimensions");
             }
         } 
     }
 
-    void SplitDimensions()
+    IEnumerator SplitDimensions()
     {
+        // Glitching Effect
+        foreach (GlitchEffects glitch in glitches) 
+        {
+            glitch.enabled = true;
+        }
+    
+        for (float ft = 0f; ft <= 5; ft += 0.1f)
+        {
+            foreach (GlitchEffects glitch in glitches) 
+            {
+                glitch.colorIntensity = ft;
+                glitch.flipIntensity = ft;
+            }
+            yield return new WaitForSeconds(.00001f);
+        }
+        foreach (GlitchEffects glitch in glitches) 
+        {
+            glitch.colorIntensity = 0;
+            glitch.flipIntensity = 0;
+            glitch.enabled = false;
+        }
+
+        // Switch Cameras
         primeCamera.enabled = false;
         frozenCamera.enabled = true;
         overgrowthCamera.enabled = true;
+        // Set new superposition state
         superPositionState = SuperPositionState.SPLIT;
     }
 
-    void JoinDimensions()
+    IEnumerator JoinDimensions()
     {
+        // Glitching Effect
+        foreach (GlitchEffects glitch in glitches) 
+        {
+            glitch.enabled = true;
+        }
+    
+        for (float ft = 0f; ft <= 5; ft += 0.1f)
+        {
+            foreach (GlitchEffects glitch in glitches) 
+            {
+                glitch.colorIntensity = ft;
+                glitch.flipIntensity = ft;
+            }
+            yield return new WaitForSeconds(.00001f);
+        }
+        foreach (GlitchEffects glitch in glitches) 
+        {
+            glitch.colorIntensity = 0;
+            glitch.flipIntensity = 0;
+            glitch.enabled = false;
+        }
+
+        // Switch Cameras
         primeCamera.enabled = true;
         frozenCamera.enabled = false;
         overgrowthCamera.enabled = false;
+        // Set new superposition state
         superPositionState = SuperPositionState.TOGETHER;
     }
 }
