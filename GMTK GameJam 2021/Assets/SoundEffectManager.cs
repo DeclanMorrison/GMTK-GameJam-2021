c20fa01c90ee7b06a214085ead7ceb6c7feae70d
Oliver Philbrick <oliveman521@gmail.com>
2021-06-13 11:56:48 -0400

    Sound effect manager

---
+++ GMTK GameJam 2021/Assets/SoundEffectManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SoundEffectManager : MonoBehaviour
+{
+    public static SoundEffectManager instance;
+    public AudioSource source;
+    public List<SoundEffect> allSoundEffects = new List<SoundEffect>();
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void Play(string name)
+    {
+        foreach(SoundEffect se in allSoundEffects)
+        {
+            if(name == se.name)
+            {
+                source.PlayOneShot(se.clips[UnityEngine.Random.Range(0, se.clips.Length)], se.volume);
+                continue;
+            }
+        }
+    }
+
+    [Serializable]
+    public class SoundEffect
+    {
+        public string name;
+        public AudioClip[] clips = new AudioClip[1]; //clips(will randomly chose 1)
+        [Range(0,1)]public float volume;
+    }
+}
