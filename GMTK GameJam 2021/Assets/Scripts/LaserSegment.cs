c5abec88c61e1b8e7cfa7c83bd85ba36d5048b80
George Philbrick <gphilbri@gmail.com>
2021-06-12 20:16:58 -0400

    Added bloom

--- GMTK GameJam 2021/Assets/Scripts/LaserSegment.cs
+++ GMTK GameJam 2021/Assets/Scripts/LaserSegment.cs
@@ -5,26 +5,37 @@ using UnityEngine;
 public class LaserSegment : MonoBehaviour
 {
     public float distanceRay = 100;
-    public Vector2 originatingPoint;
     private LineRenderer lineRenderer;
+    LayerMask interactableLayers;
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        RenderLaser();
+        interactableLayers = (1 << gameObject.layer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RenderLaser();
     }
 
     void RenderLaser()
     {
-        if (Physics2D.Raycast(transform.position, transform.right))
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, transform.right, distanceRay, interactableLayers);
+        if (hit2D)
         {
-            
+            Draw2DRay(transform.position, hit2D.point);
         }
+        else
+        {
+            Draw2DRay(transform.position, transform.right * distanceRay);
+        }
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos)
+    {
+        lineRenderer.SetPosition(0, startPos);
+        lineRenderer.SetPosition(1, endPos);
     }
 }
